c1d79f161550f6d4950f753cd64c9fdefe3f0155
KirankumarAmbati <kiran.chinna12520@gmail.com>
2019-02-27 10:31:19 +0530

    Updates

--- component---src-pages-contact-js-dce3c00d46aff2435d40.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{143:function(t,e,n){"use strict";n.r(e);var o=n(0),c=n.n(o);e.default=function(t){t.siteTitle;return c.a.createElement("div",{style:{backgroundColor:"#33334d",color:"white"}},"Contact")}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-dce3c00d46aff2435d40.js.map
