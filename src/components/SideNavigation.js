1d1b1a6dea9730cecbaf1b1912ba0b986501cd5b
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2020-08-13 12:53:51 +0530

    Update SideNavigation.js

--- src/components/SideNavigation.js
+++ src/components/SideNavigation.js
@@ -9,7 +9,7 @@ export default props => {
       <Link className="link" activeClassName="active-class" to={`/`}>home</Link>
       <Link className="link" activeClassName="active-class" to={`/blog`}>blog</Link>
       <Link className="link" activeClassName="active-class" to={`/talks`}>talks</Link>
-      <a className="link" href="https://i-can-not-react.netlify.com/">i-can-not-react</a>
+      <a className="link" href="https://i-can-not-react.netlify.app/">i-can-not-react</a>
     </Menu>
   );
 };
